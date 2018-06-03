a9d90d828f716baf4810868de38df5c2e25b94f4
Hermit <m5687946568@outlook.com>
2018-06-04 03:07:01 +0800

    Updata v1.0.0.0

--- World Boss Next Time/Form1.Designer.cs
+++ World Boss Next Time/Form1.Designer.cs
@@ -28,13 +28,159 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold);
+            this.label1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(515, 52);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Font = new System.Drawing.Font("微軟正黑體", 11.25F);
+            this.groupBox1.Location = new System.Drawing.Point(14, 59);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(624, 268);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // label3
+            // 
+            this.label3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label3.Location = new System.Drawing.Point(313, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(305, 23);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.pictureBox2.Location = new System.Drawing.Point(365, 26);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label2.Location = new System.Drawing.Point(6, 229);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(305, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.pictureBox1.Location = new System.Drawing.Point(58, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox1.HideSelection = false;
+            this.textBox1.Location = new System.Drawing.Point(14, 335);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(624, 27);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "textBox1";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.Click += new System.EventHandler(this.TextBox1Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold);
+            this.button1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.button1.Location = new System.Drawing.Point(14, 372);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(624, 35);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "複製";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold);
+            this.button2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.button2.Location = new System.Drawing.Point(533, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 52);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "更新";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(652, 421);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Bold);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Black Desert World Boss Next Time";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- World Boss Next Time/Form1.cs
+++ World Boss Next Time/Form1.cs
@@ -12,9 +12,178 @@ namespace World_Boss_Next_Time
 {
     public partial class Form1 : Form
     {
+        string tyear = "0000";
+        string tmonth = "00";
+        string tday = "00";
+        string thour = "00";
+        string tminute = "00";
+        string tweek = "0";
+        
         public Form1()
         {
             InitializeComponent();
+            NowDateTime(ref tyear, ref tmonth, ref tday, ref thour, ref tminute, ref tweek);
+            GetBossNextTime(tyear, tmonth, tday, thour, tminute, tweek);
+            GC.Collect();
+        }
+
+        public void GetBossNextTime(string todayyear, string todaymonth, string todayday, string todayhour, string todayminute, string todayweek)
+        {
+            //TodayDateTime TDT = new TodayDateTime();
+            int tHM = Convert.ToInt16(thour + tminute);
+            label1.Text = "現在時間：" + tyear + "／" + tmonth + "／" + tday + "　" + thour + "：" + tminute + "　" + GetWeekCht(tweek);
+
+
+            //出現時間
+            int[] allbosstime = { 0200, 1100, 1500, 1900, 2330 };
+            string[,] bosstimecht = new string[,]
+            {
+                    { "02", "11", "15", "19", "23" },
+                    { "00", "00", "00", "00", "30" }
+            };
+
+            //世界王名字及圖片
+            Bitmap p0 = null; string b0 = null;
+            Bitmap p1 = new Bitmap(World_Boss_Next_Time.Properties.Resources.b1); string b1 = "《○克價卡》";
+            Bitmap p2 = new Bitmap(World_Boss_Next_Time.Properties.Resources.b2); string b2 = "《●庫屯》";
+            Bitmap p3 = new Bitmap(World_Boss_Next_Time.Properties.Resources.b3); string b3 = "《★羅裴勒》";
+            Bitmap p4 = new Bitmap(World_Boss_Next_Time.Properties.Resources.b4); string b4 = "《☆卡嵐達》";
+            Bitmap p5 = new Bitmap(World_Boss_Next_Time.Properties.Resources.b5); string b5 = "《█奧平》";
+            Bitmap p6 = new Bitmap(World_Boss_Next_Time.Properties.Resources.b6); string b6 = "《▲木拉卡》";
+            Bitmap p7 = new Bitmap(World_Boss_Next_Time.Properties.Resources.b7); string b7 = "《▼肯恩特》";
+
+            //各時段出現種類名稱及圖片 array[出現時段,星期,王1王2]
+            string[,,] bossname = new string[,,]
+                {
+                    { { b3, b4 }, { b1, b2 }, { b3, b0 }, { b4, b0 }, { b2, b0 }, { b1, b3 }, { b1, b2 } },
+                    { { b1, b3 }, { b4, b0 }, { b1, b2 }, { b1, b3 }, { b2, b0 }, { b5, b0 }, { b2, b4 } },
+                    { { b2, b0 }, { b1, b2 }, { b3, b0 }, { b4, b0 }, { b1, b3 }, { b3, b4 }, { b5, b0 } },
+                    { { b3, b4 }, { b4, b0 }, { b2, b0 }, { b1, b2 }, { b3, b0 }, { b1, b2 }, { b1, b3 } },
+                    { { b5, b0 }, { b1, b2 }, { b1, b3 }, { b3, b4 }, { b2, b4 }, { b6, b7 }, { b0, b0 } }
+            };
+            Bitmap[,,] bossphoto = new Bitmap[,,]
+                {
+                    { { p3, p4 }, { p1, p2 }, { p3, p0 }, { p4, p0 }, { p2, p0 }, { p1, p3 }, { p1, p2 } },
+                    { { p1, p3 }, { p4, p0 }, { p1, p2 }, { p1, p3 }, { p2, p0 }, { p5, p0 }, { p2, p4 } },
+                    { { p2, p0 }, { p1, p2 }, { p3, p0 }, { p4, p0 }, { p1, p3 }, { p3, p4 }, { p5, p0 } },
+                    { { p3, p4 }, { p4, p0 }, { p2, p0 }, { p1, p2 }, { p3, p0 }, { p1, p2 }, { p1, p3 } },
+                    { { p5, p0 }, { p1, p2 }, { p1, p3 }, { p3, p4 }, { p2, p4 }, { p6, p7 }, { p0, p0 } }
+            };
+
+            int BTS = 0, BWS = 0;
+            BossStage(ref BTS, ref BWS);
+            BossStageChk(ref BTS, ref BWS);
+            ViewBossData(bosstimecht[0, BTS], bosstimecht[1, BTS], BWS, bossname[BTS, BWS, 0], bossname[BTS, BWS, 1], bossphoto[BTS, BWS, 0], bossphoto[BTS, BWS, 1]);
+
+            void ViewBossData(string bth, string btm, int week, string bn1, string bn2, Bitmap bp1, Bitmap bp2)
+            {
+                textBox1.Text = "€" + bth + btm + " " + bn1 + bn2;
+                groupBox1.Text = "下次世界王時間：" + tyear + "／" + tmonth + "／" + tday + "　" + bth + "：" + btm + "　" + GetWeekCht(week.ToString());
+                label2.Text = bn1;
+                label3.Text = bn2;
+                pictureBox1.Image = bp1;
+                pictureBox2.Image = bp2;
+            }
+
+            void BossStage(ref int BossTimeStage, ref int BossWeekStage)
+            {
+                //取得時段
+                int TC = allbosstime.Count() - 1;
+                for (int t=1; t <= TC; t++)
+                {
+                    if (tHM <= allbosstime[t] && tHM > allbosstime[t - 1]) { BossTimeStage = t; }
+                }
+                if (tHM <= allbosstime[0]) { BossTimeStage = 0; }
+                if (tHM > allbosstime[TC]) { BossTimeStage = 0; }
+
+                //取得星期
+                int weeknumber = Convert.ToInt16(tweek);
+                if (tHM <= allbosstime[TC]) { BossWeekStage = weeknumber; }
+                if (tHM > allbosstime[TC])
+                {
+                    if (weeknumber >= 6)
+                    {
+                        BossWeekStage = 0;
+                    }
+                    else
+                    {
+                        BossWeekStage = weeknumber + 1;
+                    }
+                }
+            }
+
+            void BossStageChk(ref int BossTimeStage, ref int BossWeekStage)
+            {
+                int TC = allbosstime.Count() - 1;
+                if (bossname[BTS, BWS, 0] == null && bossname[BTS, BWS, 1] == null)
+                {
+                    if (BossTimeStage <= TC) { BossTimeStage += 1; }
+                    if (BossTimeStage > TC)
+                    {
+                        BossTimeStage = 0;
+                        if (BossWeekStage >= 6)
+                        {
+                            BossWeekStage = 0;
+                        }
+                        else
+                        {
+                            BossWeekStage += 1;
+                        }
+                    }
+                    
+                }
+            }
+
+        }
+        
+        public void NowDateTime(ref string tyear,ref string tmonth,ref string tday,ref string thour,ref string tminute,ref string tweek)
+        {
+            tyear = DateTime.Now.ToString("yyyy");
+            tmonth = DateTime.Now.ToString("MM");
+            tday = DateTime.Now.ToString("dd");
+            thour = DateTime.Now.ToString("HH");
+            tminute = DateTime.Now.ToString("mm");
+            tweek = DateTime.Now.DayOfWeek.ToString("d");
+        }
+
+        public string GetWeekCht(string week)
+        {
+            switch (week)
+            {
+                case "0":
+                    return "星期日";
+                case "1":
+                    return "星期一";
+                case "2":
+                    return "星期二";
+                case "3":
+                    return "星期三";
+                case "4":
+                    return "星期四";
+                case "5":
+                    return "星期五";
+                case "6":
+                    return "星期六";
+                default:
+                    return "";
+            }
+        }
+
+        private void TextBox1Click(object sender, EventArgs e)
+        {
+            ((TextBox)sender).SelectAll();
+        }
+
+        private void Button1Click(object sender, EventArgs e)
+        {
+            Clipboard.SetData(DataFormats.Text, textBox1.Text);
+        }
+
+        public void Button2Click(object sender, EventArgs e)
+        {
+            NowDateTime(ref tyear, ref tmonth, ref tday, ref thour, ref tminute, ref tweek);
+            GetBossNextTime(tyear, tmonth, tday, thour, tminute, tweek);
+            GC.Collect();
         }
     }
 }
